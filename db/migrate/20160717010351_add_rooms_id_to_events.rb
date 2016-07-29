636415a5cac23d23c7f7cdde92e3997372878724
spagnolodesign <luca.spagnolo91@gmail.com>
2016-07-29 13:56:40 +0200

    remove other table

--- db/migrate/20160717010351_add_rooms_id_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddRoomsIdToEvents < ActiveRecord::Migration[5.0]
-  def change
-			add_reference :events, :chat_rooms, index: true
-  end
-end
--- db/migrate/20160717102218_remove_reference_to_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferenceToEvents < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :events, :chat_rooms_id
-  end
-end
