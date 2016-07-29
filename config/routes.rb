b03d618a2135bd96bcc0536ca0fd865a1f9f9586
spagnolodesign <luca.spagnolo91@gmail.com>
2016-07-29 14:18:57 +0200

    config actioncable production

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root to: "home#index"
   #mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 
 end
