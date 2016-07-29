1a99fd11271fe7dfd05b27fc9514b7092f0c44a8
spagnolodesign <luca.spagnolo91@gmail.com>
2016-07-29 14:00:12 +0200

    remove other table

--- db/migrate/20160717190216_create_users_rooms.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersRooms < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users_rooms, id: false do |t|
-      t.belongs_to :chat_rooms, index: true
-      t.belongs_to :users, index: true
-    end
-  end
-end
--- db/migrate/20160717193422_change_subscritpion_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeSubscritpionName < ActiveRecord::Migration[5.0]
-  def change
-	    rename_table :subscriptions, :approvals
-  end
-end
