995f000fa30adc6f4f3d26f95661481ba76c263c
spagnolodesign <luca.spagnolo91@gmail.com>
2017-05-03 00:42:39 +0200

    Add new export with more fields

--- app/grids/users_grid.rb
+++ app/grids/users_grid.rb
@@ -28,8 +28,23 @@ class UsersGrid
   column(:name, :header => "Name", :mandatory => true) do |model|
     model.name
   end
-  
 
+  column(:phone, :header => "Phone", :mandatory => true) do |model|
+    model.phone
+  end
+
+  column(:full_street_address, :header => "Address", :mandatory => true) do |model|
+    model.full_street_address
+  end
+
+  column(:gender, :header => "Gender", :mandatory => true) do |model|
+    model.gender
+  end
+
+  column(:job, :header => "Job", :mandatory => true) do |model|
+    model.job
+  end
+  
   column(:actions, :html => true, :mandatory => true) do |record|
     link_to "Delete", record, method: :delete, data: { confirm: 'Are you sure?' }
   end
