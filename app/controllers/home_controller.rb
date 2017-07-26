1bcded1598ef96c4bf267c1d827ac5900bc7cafb
Luca Spagnolo <luca@careibu.com>
2017-07-26 15:24:51 +0200

    Update hp and query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,19 @@ class HomeController < ApplicationController
 	before_filter :allow
 
 	def index
-		#@tags = User.all.tag_counts_on(:tags).as_json(only: [:name]).to_json.html_safe
 		@users = User
 							.near([current_user.latitude, current_user.longitude], 20)
 							.where(local: !current_user.local)
 							.for_age_range(current_user.age - 2, current_user.age + 2)
 							.tagged_with(current_user.tags, :any => true)
 							.where.not(id: current_user.id).order("RANDOM()").last(10)
+		if @users.empty?
+			@users = User.near([current_user.latitude, current_user.longitude], 20)
+									 .where(local: !current_user.local)
+									 .for_age_range(current_user.age - 5, current_user.age + 5)
+									 .where.not(id: current_user.id).order("RANDOM()").last(10)
+		end
+
 	end
 
 	def auth_user
