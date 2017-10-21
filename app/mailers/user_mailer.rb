e53d270e41a98aea686dfa16fc688a932ff553c2
Pedro Koike <ps.koike@gmail.com>
2017-10-21 16:25:24 -0200

    send request, accepted and rejected Blend email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,6 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: "Welcome to Blendin!")
   end
 
-  def match_email(user, room)
-    @user = user
-    @url  = chat_room_url(room)
-
-    mail(to: @user.email, subject: "You have a blend-message")
-  end
-
   def message_unread(user)
     @user = user
     mail(to: @user.email, subject: "You have a new message")
--- app/models/blend.rb
+++ app/models/blend.rb
@@ -4,4 +4,25 @@ class Blend < ActiveRecord::Base
 
   validates :message, length: { maximum: 280 }, presence: true
   validates :recipient, presence: true
+
+  after_create :send_blend_request_email
+  after_update :send_status_update_email
+
+
+
+
+  private
+
+  def send_blend_request_email
+    BlendMailer.blend_request_email(self).deliver
+  end
+
+  def send_status_update_email
+    case self.status
+      when 'accepted'
+        BlendMailer.blend_accepted_email(self).deliver
+      when 'rejected'
+        BlendMailer.blend_rejected_email(self).deliver
+    end
+  end
 end
