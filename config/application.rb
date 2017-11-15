ffc619b7c942c2a50687d64e1e6fa15d111401fb
Luca <luca@careibu.com>
2017-11-15 09:46:03 +0100

    Lang links in Nav

--- config/application.rb
+++ config/application.rb
@@ -4,20 +4,11 @@ require 'rails/all'
 require 'action_view'
 include ActionView::Helpers::DateHelper
 
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Noc
   class Application < Rails::Application
-
   	config.remove_unused_tags = true
-
     config.i18n.default_locale = :nl
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
