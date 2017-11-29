cb0fb294698b443bd359918c86c48efbb82fb544
Luca <luca@careibu.com>
2017-11-29 15:34:16 +0000

    seed for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 #Blend.create! sender_id:1248, recipient_id:40, message:"Hello I would like to connect wit you :)"
 #Blend.destroy_all
-AdminUser.create!(email: 'admin@blendinnow.nl', password: 'blendin##2017', password_confirmation: 'blendin##2017') if Rails.env.development?
+AdminUser.create!(email: 'admin@blendinnow.nl', password: 'blendin##2017', password_confirmation: 'blendin##2017')
