23fee58d06a908a20c64e37cebfaa6fca65798d3
Pedro Koike <ps.koike@gmail.com>
2018-01-02 11:52:06 -0200

    add notification icon and low cache to sql count pending blends

--- app/models/user.rb
+++ app/models/user.rb
@@ -107,4 +107,10 @@ class User < ActiveRecord::Base
     return user
   end
 
+  def self.count_pending_blend(user)
+    Rails.cache.fetch("count_pending_blends", expires_in: 10.minutes) do
+      user.received_blends.where(status: 'pending').count
+    end
+  end
+
 end
