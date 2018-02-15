b4188548030f4bdefa938a52c674966ab3b130d2
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-02-15 16:06:37 +0800

    change ENV

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -6,15 +6,15 @@ class TwilioService
     @blend = blend
     @sender = blend.sender
     @recipient = blend.recipient
-    @client = Twilio::REST::Client.new(Rails.application.secrets.TWILIO_ACCOUNT_SID, Rails.application.secrets.TWILIO_AUTH_TOKEN)
-    @from = Rails.application.secrets.TWILIO_PHONE
+    @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    @from = ENV['TWILIO_PHONE']
   end
 
   def message
     if I18n.locale == :en
-      "#{@recipient.name}, you have a new Blend request! Are you curious to know who this is? Click here! #{user_url(@sender, locale: :en, host: Rails.application.secrets.HOST)}"
+      "#{@recipient.name}, you have a new Blend request! Are you curious to know who this is? Click here! #{user_url(@sender, locale: :en, host: ENV['MAIN_URL'])}"
     else
-      "#{@recipient.name}, je hebt een nieuw Blend verzoek! Ben je benieuwd wie dit is? Klik dan hier! #{user_url(@sender, locale: :nl, host: Rails.application.secrets.HOST)}"
+      "#{@recipient.name}, je hebt een nieuw Blend verzoek! Ben je benieuwd wie dit is? Klik dan hier! #{user_url(@sender, locale: :nl, host: ENV['MAIN_URL'])}"
     end
   end
 
