8622a0ed648fa98341baf276733e106531a569d1
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-05-13 20:22:26 +0800

    unread result in conversation view

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -3,18 +3,21 @@ class InboxController < ApplicationController
 
   def index
     @conversations = current_user.mailbox.inbox
+    @messages_unread_count = current_user.mailbox.inbox({:read => false}).count
   end
 
   def show
     set_conversation
     @receipts = @conversation.receipts_for current_user
+    render :action => :show
+    @receipts.mark_as_read
   end
 
   def reply
     set_conversation
     response = params[:message][:body]
     send_message = current_user.reply_to_conversation(@conversation, response)
-    
+
     respond_to do |format|
       if send_message.save
         format.html { redirect_to inbox_path(@conversation), notice: "Message sent" }
@@ -26,7 +29,7 @@ class InboxController < ApplicationController
   end
 
   def set_conversation
-    @conversation = current_user.mailbox.inbox.find(params[:id])
+    @conversation = current_user.mailbox.inbox.order('created_at DESC').find(params[:id])
   end
 
 end
