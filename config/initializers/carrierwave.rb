496a186428f3a40eb684557414d1a0580f73d05f
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-24 13:44:42 +0800

    Protect keys on aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAIOPPK7RU4V34P6MQ',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
       :region                 => 'eu-central-1'
   }
-  config.fog_directory    = "berryhoop"
+  config.fog_directory    = ENV['S3_BUCKET_NAME'],
   config.fog_public       = false
 end
