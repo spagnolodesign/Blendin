c358664821a33f02914ce9b65ff7331be0e097d0
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-06-27 22:47:54 +0800

    text changes

--- app/assets/javascripts/event_unsubscription_modal.js
+++ app/assets/javascripts/event_unsubscription_modal.js
@@ -6,7 +6,7 @@ function generateUnsubscriptionModal(){
       button.forEach(function(element){
         element.addEventListener("click", function(){
           var body = document.querySelector("#modal-body")
-          var link = "<span id = 'confirm'>" + I18n.t('events.index.unsubscribe_modal') + "<a class='btn btn-primary mt-3' rel='nofollow' id='confirm' data-method='delete' href='/participants/" + element.dataset.value + "'>" + I18n.t('events.index.unsubscribe_button')+ "</a></span>"
+          var link = "<span id = 'confirm'>" + I18n.t('events.index.unsubscribe_modal') + "<a class='btn btn-primary mt-3' rel='nofollow' id='confirm' data-method='delete' href='/participants/" + element.dataset.value + "'>" + I18n.t('events._card_my_event.cantattend')+ "</a></span>"
           body.insertAdjacentHTML('beforeend', link)
           closeModal = document.querySelector("#close-modal")
            closeModal.addEventListener("click", function(){
